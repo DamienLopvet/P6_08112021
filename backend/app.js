21b76dd27d37a52d4dcc6473d228f522eb7359d0
Damien Lopvet <etwincorp@gmail.com>
2021-11-24 07:14:05 -0300

    add .env.exemple

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const userRoutes = require("./routes/user");
 const path = require('path');
 const helmet = require('helmet')
-require('dotenv').config()
+const dotenv = require('dotenv').config()
 
 mongoose
   .connect(
@@ -28,8 +28,9 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use(helmet())
+
 app.use(express.json());
+app.use(helmet())
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use("/api/sauces", saucesRoutes);
 app.use('/api/auth', userRoutes);
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,21 +1,17 @@
 const jwt = require('jsonwebtoken');
-const user = require('../models/user');
-
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
     const userId = decodedToken.userId;
-    const ddbUserId = user.findOne({_id : userId})._conditions._id
-    
-    if (!ddbUserId) {
+    if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
-    } 
+    } else {
       next();
-    
+    }
   } catch {
-    res.status(403).json({
-      error: new Error('unauthorized request!')
+    res.status(401).json({
+      error: new Error('Invalid request!')
     });
   }
 };
